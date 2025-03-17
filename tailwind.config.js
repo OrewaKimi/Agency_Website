04aef2a5339123879754cf531aaa8e8aed8013c8
Kimi Maulana <kimi93.com@gmail.com>
2025-03-17 10:00:52 +0700

    update colors and node modules flowbite-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
 
+  theme: {
+    extend: {
+      colors: {
+        "brandPrimary": "#4CAF4F",
+        "neutralDGrey": "#4D4D4D",
+        "neutralGrey": "#717171",
+        "neutralSilver": "#F5F7FA",
+        "gray900": "#18191F",
+      },
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+};
